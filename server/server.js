3f5aba2219be37204bfdd31436145ad360934e36
Darryl Hood <darryl.r.hood@gmail.com>
2020-07-07 18:59:42 -0400

    service of static files from dist folder added in server/server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
 import express from 'express'
+import devBundle from './devBundle' //Comment out for production
+import path from 'path'
 
 const app = express()
+const CURRENT_WORKING_DIR = process.cwd()
+devBundle.compile(app) //Comment out for production
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
