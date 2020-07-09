424f71f9ada1bc0d22a0ecad8394c00577bad31d
Darryl Hood <darryl.r.hood@gmail.com>
2020-07-09 13:15:16 -0400

    Express app configured to start server that listens on port for requests

--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
-import express from 'express'
-import devBundle from './devBundle' //Comment out for production
 import path from 'path'
+import express from 'express'
+import template from './../template'
+import devBundle from './devBundle' //Comment out for production
 
 const app = express()
-const CURRENT_WORKING_DIR = process.cwd()
 devBundle.compile(app) //Comment out for production
+
+const CURRENT_WORKING_DIR = process.cwd()
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+app.get('/', (req, res) => {
+    res.status(200).send(template())
+})
+
+let port = process.env.PORT || 3000
+app.listen(port, function onStart(err) {
+    if (err) {
+        console.log(err)
+    }
+    console.info('Server started on port %s.', port)
+})
